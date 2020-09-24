799da889c3cbbfa35d20468f6c245123f1faf193
Sandhya Neupane <iamsandhyaneupane@gmail.com>
2020-09-24 08:58:50 +0545

    Learned files and directories

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -1,21 +1,35 @@
 ﻿using LearnCollections;
+using System.IO;
+using System.Linq;
+using System;
 
 public class Program
 {
     static void Main()
     {
-        FileIO fileIO = new FileIO();
-        fileIO.LearnDirectories();
-    //     // Collections collections = new Collections();
-    //     // collections.LearnDictionary();
-    
-    //    Square square = new Square(12.4);
-    //     //square.Side = 12;
-    //     square.Display();
-    //    //var SquareArea = square.GetArea();
- 
-    //     var rect = new Rectangle(12.4 , 16.1);
-    //     var AreaRect =  rect.GetArea();
-    //     var CircRect = rect.GetCircumference();
+        var countriesText = File.ReadAllText("Countries.txt");
+        string[] countries = countriesText.Split("\r\n");
+
+        var countriesWithNInitial = countries.Where(x => x.StartsWith("N")).Select(x => x);
+
+        foreach (var country in countriesWithNInitial)
+        {
+            Console.WriteLine(country);
+        }
     }
 }
+// FileIO fileIO = new FileIO();
+// fileIO.LearnDirectories();
+
+//     // Collections collections = new Collections();
+//     // collections.LearnDictionary();
+
+//    Square square = new Square(12.4);
+//     //square.Side = 12;
+//     square.Display();
+//    //var SquareArea = square.GetArea();
+
+//     var rect = new Rectangle(12.4 , 16.1);
+//     var AreaRect =  rect.GetArea();
+//     var CircRect = rect.GetCircumference();
+
