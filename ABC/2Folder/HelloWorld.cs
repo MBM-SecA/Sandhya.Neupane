ee0cb4db27ab39dd7ee8fb46d53f98c724cbf5b9
Sandhya Neupane <iamsandhyaneupane@gmail.com>
2020-09-24 10:01:28 +0545

    added assignment

---
+++ ABC/2Folder/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+public class HelloWorld
+{
+    static void Main()
+    {
+        FileAndDirectories fileAndDirectories = new FileAndDirectories();
+        fileAndDirectories.AboutDirectories();
+
+        Console.WriteLine("Hello World");
+    }
+}
---
+++ Assignment 2/Question.cs
@@ -0,0 +1,20 @@
+// Q.2. Create a folder "ABC", create 10 subfolders in "ABC". Each
+// subfolder contain a C# file with hello world program.
+
+
+using System;
+using System.Linq;
+using System.IO;
+public class FileAndDirectories
+{
+    public void AboutDirectories()
+    {
+        Directory.CreateDirectory(@".../ABC");
+        string fileHelloWorld = File.ReadAllText("HelloWorld.cs");
+        for (int i = 0; i < 10; i++)
+        {
+            Directory.CreateDirectory($"../ABC/{i + 1}Folder");
+            File.WriteAllText($"../ABC/{i + 1}Folder/HelloWorld.cs", fileHelloWorld);
+        }
+    }
+}
