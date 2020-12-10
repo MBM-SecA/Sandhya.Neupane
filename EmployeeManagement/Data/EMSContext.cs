5d6a51383bf2dfbf5ad1ad96a81faba7ca206d66
Sandhya Neupane <iamsandhyaneupane@gmail.com>
2020-12-10 19:33:20 +0545

    Updated

---
+++ EmployeeManagement/Data/EMSContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EmployeeManagement.Data
+{
+    public class EMSContext : DbContext
+    {
+        public EMSContext()
+        {
+
+        }
+        public EMSContext(DbContextOptions<EMSContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+        // public DbSet<Department> Department { get; set;}
+    }
+}
